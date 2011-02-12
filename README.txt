d3fd27d75b1f083faf800002eb6562209a64c03d
William Chambers <william@bioselement.com>
2011-02-11 23:03:11 -0500

    Pyramid routesalchemy 1.0 paster template.

---
+++ README.txt
@@ -0,0 +1,5 @@
+
+BEAuth README
+=============
+
+BEAuth or "Bios Element's Authentication" is a Pyramid-based user auth system.
---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'repoze.tm2>=1.0b1', # default_commit_veto
+    'zope.sqlalchemy',
+    'WebError',
+    ]
+
+if sys.version_info[:3] < (2,5,0):
+    requires.append('pysqlite')
+
+setup(name='BEAuth',
+      version='0.0',
+      description='BEAuth',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='William Chambers',
+      author_email='FIRSTNAME@bioselement.com',
+      url='http://bioselement.com/',
+      keywords='web wsgi pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='beauth',
+      install_requires = requires,
+      entry_points = """\
+      [paste.app_factory]
+      main = beauth:main
+      """,
+      paster_plugins=['pyramid'],
+      )
+
