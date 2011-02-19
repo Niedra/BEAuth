2a4a0909eaa2f41c277b90daaebcd12969f7116f
William Chambers <william@bioselement.com>
2011-02-18 19:49:11 -0500

    Added ViewRoot test.

--- beauth/tests.py
+++ beauth/tests.py
@@ -8,6 +8,19 @@ def _initTestingDB():
     session = initialize_sql(create_engine('sqlite://'))
     return session
 
+class ViewRootTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+        
+    def test_it(self):
+        from beauth.views import root
+        request = testing.DummyRequest()
+        response = root(request)
+        self.assertEqual(response['project'], u'BEAuth')
+
 class ViewUserTests(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
