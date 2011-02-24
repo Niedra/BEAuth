3081ebdd08f6f31acd5216673bf9fb2e829caa71
William Chambers <william@bioselement.com>
2011-02-24 14:51:00 -0500

    Defined strings as unicode.

--- beauth/forms/user.py
+++ beauth/forms/user.py
@@ -1,12 +1,12 @@
 from wtforms import Form, BooleanField, PasswordField, SubmitField, TextField, validators
 
 class RegistrationForm(Form):
-    name         = TextField('Username', [validators.Length(min=4, max=25)])
-    password     = PasswordField('Password', [validators.Length(min=4, max=25)])
-    email        = TextField('Email Address', [validators.Length(min=6, max=35)])
-    accept_rules = BooleanField('I accept the site rules', [validators.Required()])
+    name         = TextField(u'Username', [validators.Length(min=4, max=25)])
+    password     = PasswordField(u'Password', [validators.Length(min=4, max=25)])
+    email        = TextField(u'Email Address', [validators.Length(min=6, max=35)])
+    accept_rules = BooleanField(u'I accept the site rules', [validators.Required()])
 
 class LoginForm(Form):
-    name         = TextField('Username', [validators.Length(min=4, max=25)])
-    password     = PasswordField('Password', [validators.Length(min=4, max=25)])
-    submit       = SubmitField('Submit')
+    name         = TextField(u'Username', [validators.Length(min=4, max=25)])
+    password     = PasswordField(u'Password', [validators.Length(min=4, max=25)])
+    submit       = SubmitField(u'Submit')
